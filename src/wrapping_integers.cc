05c04d068351153426e455f911b432b8464fb738
hiidy <hiidy115@gmail.com>
2025-07-08 08:57:32 +0000

    feat: Wrap32의 wrap기능 구현

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,13 +1,16 @@
 #include "wrapping_integers.hh"
 #include "debug.hh"
 
+#include <iostream>
+
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
   debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  uint32_t result = (zero_point.raw_value_ + n % (1ULL << 32));
+  return Wrap32( result );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
